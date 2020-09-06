cc2b57184dfd0d4b7b3b3464be3197d55d5dcf8f
TheGreatIAm1906 <richrowlandjr@gmail.com>
2020-09-05 21:31:57 -0400

    Updates

---
+++ precache-manifest.ec6a6893ee92117768b5067743ff4e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985adf0296b23f5ff241f34c107264ad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f24cfd9ee90f41cf337d",
+    "url": "/monsters-rolodex/static/css/main.92cd3622.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24cfd9ee90f41cf337d",
+    "url": "/monsters-rolodex/static/js/main.1991f8a8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
